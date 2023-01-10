8da8be8a53689fc2f62dfeebab023c286d952633
whs465 <whs465@gmail.com>
2023-01-10 13:12:02 +0400

    Complete Home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -12,7 +13,6 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
 function App() {
   return (
     <>
@@ -26,15 +26,16 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/offers" element={<Offers />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
-          <Route path="/create-listing" element={<PrivateRoute />}>
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
-          <Route path="/edit-listing" element={<PrivateRoute />}>
+          <Route path="edit-listing" element={<PrivateRoute />}>
             <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
