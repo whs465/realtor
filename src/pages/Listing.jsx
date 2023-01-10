28356f5e6ebf83b9debe317aa13d046f5fb00b87
whs465 <whs465@gmail.com>
2023-01-10 11:47:02 +0400

    add map to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -71,6 +71,7 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+
       <div
         className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
         onClick={() => {
@@ -85,12 +86,12 @@ export default function Listing() {
       </div>
       {shareLinkCopied && (
         <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
-          Link Copied
+          Link Copied!
         </p>
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className=" w-full ">
+        <div className="w-full h-[200px] lg-[400px]">
           <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - ${" "}
             {listing.offer
